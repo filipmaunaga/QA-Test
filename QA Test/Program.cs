1f22ff32eecac39dcafd1bc602372c12c49d093d
fico <fico@maher>
2019-11-25 21:22:16 +0100

    test zavrsen

--- QA Test/Program.cs
+++ QA Test/Program.cs
@@ -16,12 +16,32 @@ namespace QA_Test
             //Ukoliko su oba broja neparna, sabrati ih, i ispisati rezultat
             //Ukoliko je jedan paran, a jedan neparan, podeliti veći broj manjim
 
-            Console.WriteLine("Unesi prvi broj: ");
-            double broj1 = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("Unesi drugi broj: ");
-            double broj2 = Convert.ToDouble(Console.ReadLine());
-            double result = Zadatak1(broj1, broj2);
-            Console.WriteLine("Rezultat je {0}", result);
+            //Console.WriteLine("Unesi prvi broj: ");
+            //double broj1 = Convert.ToDouble(Console.ReadLine());
+            //Console.WriteLine("Unesi drugi broj: ");
+            //double broj2 = Convert.ToDouble(Console.ReadLine());
+            //double result = Zadatak1(broj1, broj2);
+            //Console.WriteLine("Rezultat je {0}", result);
+            //            Korisnik unosi brojeve sa tastature, sve dok ne upiše broj manji od 0.
+            //Sve te brojeve smeštati u listu brojeva. Napisati program koji će da ispiše:
+            //Sumu svih unetih brojeva
+            //Aritmetičku sredinu svih unetih brojeva
+            //Sortiranu listu brojeva, od najvećeg ka najmanjem broju
+            //Zadatak2();
+//            Korisnik unosi neki tekst sa tastature. Napisati klasu StringFunctions, 
+            //koja će da sadrži metode koji na unetom tekstu:
+//            Broji reči(metod CountWords)
+//Broji razmake(metod CountSpaces)
+//Ispisuje najkraću reč(metod ShortestWord)
+//Ispisuje najdužu reč(metod LongestWord)
+
+            
+            string tekst = "Ovo je neki tekst.";
+            int count1 = StringFunctions.CountWords(tekst);
+            int count2=StringFunctions.CountSpaces(tekst);
+            Console.WriteLine("Broj reci je: {0}", count1);
+
+            Console.WriteLine("Broj razmaka je: {0}",count2);
             Console.ReadKey();
 
         }
@@ -56,6 +76,56 @@ namespace QA_Test
             }
             return result;
 
+        }
+        static void Zadatak2()
+        {
+            List<int> lista = new List<int>();
+            bool stayinloop = true;
+            string userentry;
+            int entry;
+            do
+            {
+                Console.WriteLine("Unesi broj, za prekid unesi broj manji od nule");
+                userentry = Console.ReadLine();
+                entry = Convert.ToInt32(userentry);
+
+                if (entry<0)
+                {
+                    stayinloop = false;
+                    break;
+                }
+                lista.Add(entry);
+            } while (stayinloop);
+            Console.WriteLine("Suma svih unetih brojeva je: {0}", SumOfList(lista));
+            Console.WriteLine("Aritmeticka sredina unetih brojeva je: {0}", MeanOfList(lista));
+            Console.WriteLine("Lista brojeva od najmanjeg ka najvecem broju: ");
+            lista.Sort();
+            foreach(int i in lista)
+            {
+                Console.WriteLine(i);
+            }
+        }
+        static int SumOfList(List<int> anylist)
+        {
+
+            int sum = 0;
+            foreach (int i in anylist)
+            {
+                sum = sum + i;
+            }
+            return sum;
+        }
+        static int MeanOfList(List <int> anylist)
+        {
+            int sum = 0;
+            int result = 0;
+            foreach(int i in anylist)
+            {
+                sum = sum + i;
+                
+            }
+            result = sum / anylist.Count();
+            return result;
         }
     }
 }
---
+++ QA Test/StringFunctions.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QA_Test
+{
+    class StringFunctions
+    {
+        public static int CountWords(string tekst)
+        {
+            int count = 0;
+            bool inWord = false;
+
+            foreach (char t in tekst)
+            {
+                if (char.IsWhiteSpace(t))
+                {
+                    inWord = false;
+                }
+                else
+                {
+                    if (!inWord) count++;
+                    inWord = true;
+                }
+            }
+            return count;
+
+        }
+        public static int CountSpaces(string tekst)
+        {
+            int count = 0;
+            string str1;
+            for (int i = 0; i < tekst.Length; i++)
+            {
+                str1 = tekst.Substring(i, 1);
+                if (str1 == " ")
+                    count++;
+            }
+            return count;
+        }
+    }
+}
